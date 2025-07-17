e70f78a08bb7340699fe99e822ef2ea0007aaba2
Nico-DM <nicomgaletto@gmail.com>
2025-07-17 15:46:52 +0000

    added fixed framerate

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0 # delta time
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # ms to s
 
 
 if __name__ == "__main__":
