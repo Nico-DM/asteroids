c0d62ed7d33e57d08e9840947d3dd4649012d49d
Nico-DM <nicomgaletto@gmail.com>
2025-07-17 16:19:42 +0000

    implemented sprite groups

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     dt = 0 # delta time
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable) # type: ignore
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     # Game loop
@@ -16,12 +21,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-        
+
         dt = clock.tick(60) / 1000 # ms to s
 
 
